3db756c09e7d7ed7aac1c100c8314d3ac1157101
varun <varunannepu123@gmail.com>
2023-12-27 19:37:02 +0530

    docdb cluster

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "main" {
 
   for_each  = var.parameters
   name      = each.key
-  type      = each.value["string"]
+  type      = each.value["type"]
   value     = each.value["value"]
 
 }
